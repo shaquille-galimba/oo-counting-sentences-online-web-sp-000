93867e58654bde5b9fecbc13e10022305b06b084
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-20 09:11:12 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count_arr = self.split(/[.!?]/)
+    count_arr.delete_if{|e| e.length == 0}
+    count_arr.count
   end
 end
